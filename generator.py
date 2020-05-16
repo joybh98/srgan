b8fb6b6d5f180d1753f5fc364d68160a5adcc3f0
Joy Bhalla <joybhalla9@gmail.com>
2020-05-16 23:22:30 +0530

    Generator (#19)
    * added basic generator
    
    * generator method written
    
    * added SubPixelConv2D method, completed generator model
    
    * added prelu layer
    
    * generator working
    
    * added lines to print shape of layers

---
+++ generator.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import tensorlayer as tl
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D,BatchNormalization,Input,PReLU
+from operations import SubPixelConv2d
+
+def make_generator():
+	
+	w_init = tf.random_normal_initializer(stddev=0.02)
+	g_init = tf.random_normal_initializer()
+
+	x_in = Input(shape=(64,64,3)) # Shape : (None,64,64,3)
+
+	print(" x_in shape ", x_in.shape)
+
+	n = Conv2D(filters=64,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu', \
+		kernel_initializer=w_init,data_format="channels_last",input_shape=(64,64,3))(x_in) # TODO: Fix shape current shape: (?,60,60,64)
+	n = PReLU()(n)
+	
+	temp = n
+	
+	print(" temp shape ", temp.shape)
+
+	for i in range(15):
+		nn = Conv2D(filters=64,kernel_size=(1,1),strides=(1,1),padding='same', \
+		kernel_initializer=w_init,data_format="channels_last",input_shape=(64,64,3))(n)
+
+		print(" inside residual block shape ", nn.shape)
+
+		nn = BatchNormalization(gamma_initializer=g_init)(nn)
+		nn = PReLU()(nn)
+		nn = Conv2D(filters=64,kernel_size=(1,1),strides=(1,1),padding='same',activation='relu', \
+		kernel_initializer=w_init,data_format="channels_last",input_shape=(64,64,3))(nn)
+		nn = BatchNormalization(gamma_initializer=g_init)(nn)
+		nn = tf.add_n([n,nn])
+		n = nn
+		print(" nn inside resd block shape ", nn.shape)
+
+	print(n.shape)
+
+	n = Conv2D(filters=64,kernel_size=(1,1),strides=(1,1))(n)
+	n = BatchNormalization()(n)
+	n = tf.add_n([n,temp])
+	
+	print(" n resd block end shape ", n.shape)
+
+	n = Conv2D(filters=256,kernel_size=(1,1),strides=(1,1))(n)
+	n = SubPixelConv2d(input_shape=(n.shape),scale=2)(n)
+	n = PReLU()(n)
+	
+	print(n.shape)
+
+	n = Conv2D(filters=256,kernel_size=(1,1),strides=(1,1))(n)
+	n = SubPixelConv2d(input_shape=(n.shape), scale=2)(n)
+	n = PReLU()(n)
+
+	print(n.shape)
+
+	# Output Layer
+	nn = Conv2D(filters=3,kernel_size=(3,3),strides=(1,1),padding='same',activation='tanh')(n)
+	
+	print(" o/p layer ", nn.shape)
+	gen = tf.keras.Model(inputs=x_in,outputs=nn)
+
+generator = make_generator()
+generator.summary()
---
+++ operations.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.nn import depth_to_space
+from tensorflow.keras.layers import Lambda
+
+""" Operations which are going to be used in the model frequently or more than once """
+
+""" Pixel Shuffle """
+
+""" Convert shape(N,C,H,W) where N: batch_size, C: channel, H: height, W: width to:
+(N,C/r*r,H*r,W*r) where r is the shuffling factor. 
+Goal is to convert the depth(channel) into space(height and width) """
+
+""" 
+    Subpixel convulution
+    :param input_shape: tensor_shape, (N,C,H,W)
+    :param scale: Upsample factor
+
+"""
+
+def SubPixelConv2d(input_shape,scale:int):
+    # upsample using depth to space
+        def refactor(input_shape):
+            # operation with the r i.e shuffling factor
+            dims = [input_shape[0],
+                    input_shape[1] * scale,
+                    input_shape[2] * scale,
+                    int(input_shape[3] / (scale ** 2))]
+            # Output tuple with updated values
+            output_shape = tuple(dims)
+            return output_shape
+        
+        def subpixel(x):
+            return tf.nn.depth_to_space(x,scale)
+        
+        return Lambda(subpixel,output_shape=refactor, name='Subpixel')
