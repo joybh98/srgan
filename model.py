84d5ba3e0441a83d1bff557966f4e430f311d801
Joy Bhalla <Joy Bhalla>
2020-05-24 22:14:39 +0530

    Added generator model

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Input,Conv2D,PReLU,BatchNormalization
+from tensorflow.keras.layers import Input, Conv2D, PReLU, BatchNormalization
+from tensorflow.keras.models import Model
 
 # B residual blocks are used in the generator
 # the output of the PReLU is fed to the elementwise layer of the 1st block
 # the output of each block is then fed to the next block's elemwsise layer
+
 
 def identity_block(nn):
     # save the input value
@@ -15,15 +17,30 @@ def identity_block(nn):
     n = Conv2D()
     n = BatchNormalization()
     # elementwise sum
-    tf.math.add([n,nn])
+    tf.math.add([n, nn])
+
 
 def gen(input_shape):
-    
-    x_in = Input(shape=input_shape,name='Input')
-    nn = Conv2D(filters=64,kernel_size=9,strides=1)(x_in)
+
+    x_in = Input(shape=input_shape, name='Input')
+    nn = Conv2D(filters=64, kernel_size=9, strides=1)(x_in)
     nn = PReLU()(nn)
 
     # residual blocks
     for i in range(3):
         identity_block(nn)
-    
+
+    nn = Conv2D(filters=256, kernel_size=3, strides=1)(nn)
+    nn = BatchNormalization()
+    nn = PReLU()
+
+    nn = Conv2D(filters=256, kernel_size=3, strides=1)(nn)
+    nn = BatchNormalization()
+    nn = PReLU()
+
+    # output layer
+    output = Conv2D(filters=3, kernel_size=9, strides=1)
+
+    make_gen = Model(inputs=x_in, outputs=output)
+
+    return make_gen
